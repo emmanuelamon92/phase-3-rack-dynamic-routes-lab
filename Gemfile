4337a3711cae398d9b4fcaa8aebd6fe1f3b1d55a
Emmanuel <emmanuelamon92@gmail.com>
2021-07-15 11:42:27 -0400

    commit tree 5d9f9cba16f00e31e9165c9e5ecd34030a829573

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
+require 'pry'
 source 'https://rubygems.org'
 
 gem 'rack'
 gem 'rack-test'
 gem 'rspec'
+gem 'pry'
---
+++ app/application.rb
@@ -0,0 +1,35 @@
+
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+        # status_code = 200
+        content_type = {'Content-Type' => 'text/html'}
+        # data = ['Hello World!']
+        if req.path.include?("/items/")
+            item_name = req.path.split("/").last
+            item = @@items.find{|i| i.name == item_name}
+            if item
+                
+                # return [200, content_type, [item.price]]
+                resp.status = 200
+                resp.write(item.price)
+            else
+                return [400, content_type, ["Item not found"]]
+                resp.status = 400
+                resp.write("Item not found")
+            end
+            # binding.pry
+        else
+            return [404, content_type, ["Route not found"]]
+            resp.status = 404
+            resp.write("Route not found")
+        end
+        # binding.pry
+        resp.finish
+        # return [status_code, content_type, data]
+
+    end
+
+end
